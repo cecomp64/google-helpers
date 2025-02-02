d4d394b0bedec08e9ac03b92843affe82ea0939c
Carl Svensson <csvensson@apple.com>
2025-02-02 10:35:20 -0800

    group to groups

--- src/shared-variables.js
+++ src/shared-variables.js
@@ -1,7 +1,7 @@
 scopes = [
       'https://www.googleapis.com/auth/spreadsheets', 
-      'https://www.googleapis.com/auth/admin.directory.group',
-      'https://www.googleapis.com/auth/admin.directory.group.member',
+      'https://www.googleapis.com/auth/admin.directory.groups',
+      'https://www.googleapis.com/auth/admin.directory.groups.member',
       'https://www.googleapis.com/auth/admin.directory.user.security',
       'https://www.googleapis.com/auth/admin.directory.user',
     ];
