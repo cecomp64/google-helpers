90483a72f759d6a64d0bb18c754e3b6087b7e9c3
Carl Svensson <csvensson@apple.com>
2025-02-05 22:46:37 -0800

    it works

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { lookup_group_members, read_spreadsheet, write_spreadsheet, google_auth, google_impersonate } = require('./src/google-apis');
+const { base64_encode, add_group_members, delete_group_members, lookup_group_members, read_spreadsheet, write_spreadsheet, google_auth, google_impersonate } = require('./src/google-apis');
 
 module.exports = {
-  lookup_group_members, read_spreadsheet, write_spreadsheet, google_auth, google_impersonate
+  base64_encode, lookup_group_members, read_spreadsheet, write_spreadsheet, google_auth, google_impersonate, add_group_members, delete_group_members
 }
